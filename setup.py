183c972ac226bf022a0da32af43da3d9cebf9bb8
Golnazmes <gmesbahi.gm@gmail.com>
2024-03-28 14:08:39 -0600

    update the setup.py file with versions

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup
 
-setup(name='gym_puddle',
-      version='0.0.2', #compatible with the new gymnasium
-      install_requires=['gymnasium', 'pygame']  # And any other dependencies we need
+setup(
+    name="gym_puddle",
+    version="0.0.2",  # compatible with the new gymnasium
+    install_requires=[
+        "gymnasium==0.29.1",
+        "numpy==1.26.4",
+        "pygame==2.5.2",
+    ],  # And any other dependencies we need
+    python_requires=">=3.11",
 )
