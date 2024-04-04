226ecfbca07cf8fe82e7e8489d39f4afe58b898a
Golnazmes <gmesbahi.gm@gmail.com>
2024-04-04 11:48:43 -0600

    merge the init files for registration

--- gym_puddle/__init__.py
+++ gym_puddle/__init__.py
@@ -2,5 +2,5 @@ from gymnasium.envs.registration import register
 
 register(
     id='PuddleWorld-v0',
-    entry_point='gym_puddle.env:PuddleEnv',
+    entry_point='gym_puddle.env.puddle_env:PuddleEnv',
 )
--- gym_puddle/env/__init__.py
+++
@@ -1,1 +0,0 @@
-from gym_puddle.env.puddle_env import PuddleEnv
