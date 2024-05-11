9fa38d13a66545526a02bae9b0c6f6b668eeee9f
psaunderualberta <psaunder@ualberta.ca>
2024-05-10 21:59:38 -0600

    Comments added to q_learning

--- gym_puddle/tabular/q_learning.py
+++ gym_puddle/tabular/q_learning.py
@@ -19,7 +19,15 @@ for i in range(__NUM_ACTIONS):
 
 
 @njit
-def q_create_table(granularity: float = 10):
+def q_create_table(granularity: float = 10) -> np.ndarray:
+    """Create a Q-table for the Q-learning algorithm.
+
+    Args:
+        granularity (float, optional): The number of cells along each dimension in the Q table. Defaults to 10.
+
+    Returns:
+        np.ndarray: A Q-table with shape (granularity, granularity, __NUM_ACTIONS)
+    """
     tbl = np.zeros((granularity, granularity, __NUM_ACTIONS), dtype=np.float64)
 
     n = granularity
@@ -34,7 +42,7 @@ def q_create_table(granularity: float = 10):
                 # The goal is always in the top-right, so
                 # initalize the q table using distance to the
                 # upper-left corner. 20 steps to go across whole map,
-                # so normalize according to that
+                # so normalize according to that value.
                 tbl[i, j, a] = -np.linalg.norm((new_pos - __GOAL), ord=1) * 20
 
     return tbl
@@ -42,6 +50,15 @@ def q_create_table(granularity: float = 10):
 
 @njit
 def q_get_action(q_table: np.ndarray, pos: np.ndarray) -> int:
+    """Get the action with the highest Q-value at a given position.
+
+    Args:
+        q_table (np.ndarray): The Q-table.
+        pos (np.ndarray): The position in the puddle-world to get the action for.
+
+    Returns:
+        int: The action with the highest Q-value at the given position.
+    """
     (y, x) = q_get_position(q_table, pos)
     return np.argmax(q_table[y, x, :])
 
@@ -56,8 +73,22 @@ def q_rollout(
     puddle_width: np.ndarray,
     puddle_env_ids: np.ndarray,
     start: np.ndarray = None,
-):
+) -> tuple[int, float]:
+    """Rollout the Q-learning algorithm for a single episode.
 
+    Args:
+        q_table (np.ndarray): The Q-table to update.
+        exploration_factor (float): The probability of taking a random action.
+        lr (float): The learning rate.
+        gamma (float): The discount factor.
+        puddle_top_left (np.ndarray): The top-left corners of the puddles.
+        puddle_width (np.ndarray): The widths of the puddles.
+        puddle_env_ids (np.ndarray): The environment IDs of the puddles.
+        start (np.ndarray, optional): . Defaults to None.
+
+    Returns:
+        tuple[int, float]: The number of steps taken and the cumulative reward.
+    """
     if start is None:
         pos = np.random.uniform(0, 1, size=2)
     else:
@@ -67,11 +98,16 @@ def q_rollout(
     num_steps = 0
     while not is_done(pos, __GOAL, __GOAL_THRESHOLD):
 
+        # Get the position in the Q-table
         (yt, xt) = q_get_position(q_table, pos)
+
+        # Get the action to take, using epsilon-greedy
         if np.random.random() < exploration_factor:
             action = np.random.randint(__NUM_ACTIONS)
         else:
             action = q_get_action(q_table, pos)
+
+        # Get the reward and the next position
         pos = get_new_pos(pos, __ACTIONS, action, __THRUST_NOISE)
         reward = get_reward(
             pos,
@@ -82,8 +118,10 @@ def q_rollout(
             puddle_env_ids,
         )
 
+        # Get `pos` in the Q-table
         (ytp1, xtp1) = q_get_position(q_table, pos)
 
+        # Different update rule if we are at the goal
         if is_done(pos, __GOAL, __GOAL_THRESHOLD):
             q_table[yt, xt, action] = (1 - lr) * q_table[yt, xt, action] + lr * reward
         else:
@@ -91,6 +129,7 @@ def q_rollout(
                 reward + gamma * np.max(q_table[ytp1, xtp1, :])
             )
 
+        # Increment trackers
         num_steps += 1
         cum_reward += reward
 
@@ -102,13 +141,16 @@ def q_rollout(
 
 
 @njit
-def q_get_action(q_table: np.ndarray, pos: np.ndarray) -> int:
-    (x, y) = q_get_position(q_table, pos)
-    return np.argmax(q_table[x, y, :])
+def q_get_position(q_table: np.ndarray, pos: np.ndarray) -> tuple[int, int]:
+    """Get the position in the Q-table corresponding to a given position in the puddle-world.
 
+    Args:
+        q_table (np.ndarray):
+        pos (np.ndarray): The position in the puddle-world.
 
-@njit
-def q_get_position(q_table: np.ndarray, pos: np.ndarray) -> np.ndarray:
+    Returns:
+        tuple[int, int]: The position in the Q-table.
+    """
     n = q_table.shape[0]
     y = min(int(pos[0] * n), n - 1)
     x = min(int(pos[1] * n), n - 1)
@@ -116,6 +158,15 @@ def q_get_position(q_table: np.ndarray, pos: np.ndarray) -> np.ndarray:
 
 
 def q_upscale_table(q_table: np.ndarray, upscale_factor: int) -> np.ndarray:
+    """Upscale a Q-table by a given factor.
+
+    Args:
+        q_table (np.ndarray): The Q-table to upscale.
+        upscale_factor (int): The factor to upscale the Q-table by.
+
+    Returns:
+        np.ndarray: The upscaled Q-table.
+    """
     n = q_table.shape[0]
     return cv2.resize(
         q_table, (n * upscale_factor, n * upscale_factor), interpolation=cv2.INTER_LINEAR
--- tabular_q_train.py
+++ tabular_q_train.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 
+
 __file_location = os.path.abspath(os.path.dirname(__file__))
 __PATH2CONFIGS = os.path.join(__file_location, "gym_puddle", "env_configs")
 
@@ -24,13 +25,14 @@ def get_environment(envno: int):
 
 
 def train(config: dict) -> np.ndarray:
-    """_summary_
+    """
+    Train the Q table on the given environments
 
     Args:
-        config (_type_): _description_
+        config (dict): Configuration dictionary with the following keys:
 
     Returns:
-        np.ndarray: _description_
+        np.ndarray: Mean reward of the trained Q table
     """
     base_nrollouts = config["nrollouts"]
     nupscales = config["nupscales"]
@@ -40,7 +42,7 @@ def train(config: dict) -> np.ndarray:
 
     # Init wandb
     if config["wandb"]:
-        wandb.init(project="upper-bound-2024-comp", entity="psaunder")
+        wandb.init(project=config["wandb-project"], entity=config["wandb-entity"])
         wandb.config.update(config)
 
     env_configs = []
@@ -59,11 +61,16 @@ def train(config: dict) -> np.ndarray:
     puddle_width = np.array(puddle_width)
     puddle_ids = np.array(puddle_ids)
 
-    exploration_factor = 0.3
-    lr = 0.1
-    gamma = 1.0
+    exploration_factor = config["exploration_factor"]
+    lr = config["lr"]
+    gamma = config["gamma"]
 
-    start_pos = map(float, config["start_pos"].split(",")) if config["start_pos"] is not None else None
+    # Check if a starting position is provided, else None
+    start_pos = (
+        map(float, config["start_pos"].split(","))
+        if config["start_pos"] is not None
+        else None
+    )
     start_pos = np.array(list(start_pos)) if start_pos is not None else None
 
     steps = []
@@ -74,7 +81,9 @@ def train(config: dict) -> np.ndarray:
     for i in range(nupscales):
         if q_table is None:
             # Load table if it exists, else create a new one
-            if config["q_table_path"] is not None and os.path.isfile(config["q_table_path"]):
+            if config["q_table_path"] is not None and os.path.isfile(
+                config["q_table_path"]
+            ):
                 q_table = q_load_data(config["q_table_path"])
             else:
                 q_table = q_create_table(init_granularity)
@@ -84,20 +93,28 @@ def train(config: dict) -> np.ndarray:
             q_save_data(q_table, svpth)
 
             # Upscale table
-            q_table = q_upscale_table(q_table)
-        print(
-            f"Upscale {i} - Q Table granularity {q_table.shape[0]} x {q_table.shape[1]}"
-        )
+            q_table = q_upscale_table(q_table, config["upscale_factor"])
 
+        # Train the Q table
         pb = tqdm(range(base_nrollouts), total=base_nrollouts)
+        print(
+            f"Upscale {i} - Q Table granularity "
+            + f"{q_table.shape[0]} x {q_table.shape[1]}"
+        )
         for j in pb:
+
+            # Log information to wandb every 10000 steps
+            # Wandb doesn't like doing it super frequently
             if config["wandb"] and j % 10000 == 0:
                 if len(steps) != 0:
+                    # Log average of last 1000 steps and rewards
                     msteps = round(np.mean(steps), 2)
                     mrewards = round(np.mean(rewards), 2)
                     wandb.log({f"num_steps-{q_table.shape[0]}": msteps})
                     wandb.log({f"cum_rewards-{q_table.shape[0]}": mrewards})
 
+                    # Log average of 500 rollouts from the test's start state
+                    # No exploration
                     cum_rewards = 0
                     start = np.array([0.2, 0.4])
                     for env in envs:
@@ -106,6 +123,8 @@ def train(config: dict) -> np.ndarray:
                         puddle_width_tmp = np.array(setup["puddle_width"])
                         puddle_ids_tmp = np.array([0] * len(puddle_top_left))
 
+                        # Not exactly, but approximately equal to the 100 seeds
+                        # when evaluating the learned model
                         for _ in range(100):
                             _, cum_reward = q_rollout(
                                 q_table,
@@ -122,6 +141,7 @@ def train(config: dict) -> np.ndarray:
                         {f"cum_rewards-test-{q_table.shape[0]}": cum_rewards / 500}
                     )
 
+                # Plot the actions table
                 fig = plt.imshow(
                     np.rot90(np.argmax(q_table, axis=2)),
                     cmap="hot",
@@ -143,30 +163,40 @@ def train(config: dict) -> np.ndarray:
                 puddle_ids,
                 start_pos,
             )
+
+            # Store the last 1000 steps and rewards
             steps.append(num_steps)
             rewards.append(cum_rewards)
             steps = steps[-1000:]
             rewards = rewards[-1000:]
 
+            # Set the progress bar description
             msteps = round(np.mean(steps), 2)
             mrewards = round(np.mean(rewards), 2)
             pb.set_description_str(f"Steps: {msteps} | Rewards: {mrewards}")
 
+    # Save the final Q table
     svpth = f"{q_table_savepath}.npy"
     q_save_data(q_table, svpth)
-    df, _ = evaluate(svpth)
+    df = evaluate(svpth)
 
+    # Log the final mean reward to wandb
     if config["wandb"]:
         wandb.log({"final_mean_reward": df.mean().mean()})
-    return df.mean()
+    return df.mean().mean()
 
 
 def evaluate(filepath, save=False):
+    """
+    Evaluate the Q table on the 5 environments
+
+    Args:
+        filepath (str): Path to the Q table
+        save (bool): Whether to save the evaluation results
+    """
     q_table = q_load_data(filepath)
 
-    # Create an empty list to store the frames
-    frames = []
-
+    # Create a dataframe to store the result
     df = pd.DataFrame(
         index=range(1, 101),
         columns=[
@@ -189,56 +219,96 @@ def evaluate(filepath, save=False):
 
             while (
                 not done
-            ):  # to avoid infinite loops for the untuned DQN we set a truncation limit, but you should make your agent sophisticated enough to avoid infinite-step episodes
+            ):  # to avoid infinite loops for the untuned DQN, use a truncation limit
                 num_steps += 1
 
                 if num_steps > 10000:
+                    # This should never happen with a well-trained model
                     print("ERROR: Exceeded 10000 steps!")
                     total_reward = np.inf
                     break
 
                 action = q_get_action(q_table, obs)
-                obs, reward, done, trunc, info = env.step(action)
+                obs, reward, done, _, _ = env.step(action)
                 total_reward += reward
 
                 if done:
                     print(f"total reward in this episode: {total_reward}")
                     break
             df.loc[seed, f"ep_reward_pw{envno}"] = total_reward
-            print(df.loc[seed, f"ep_reward_pw{envno}"])
 
             env.close()
 
     print(df.mean(), df.mean().mean())
     df = df.reset_index(names=["seed_ID"])
 
+    # Save the evaluation results
     if save:
         df.to_csv(f"dp/eval.csv", index=False)
 
-    return df, frames
+    return df
 
 
 if __name__ == "__main__":
+    # Define the command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--nrollouts", type=int, default=int(1e6), help="Number of rollouts to perform per upscaled table")
-    parser.add_argument("--nupscales", type=int, default=8, help="Number of times to upscale the table")
-    parser.add_argument("--upscale-factor", type=int, default=2, help="Factor to upscale the table by")
-    parser.add_argument("--lr", type=float, default=0.01, help="Learning rate for Q learning")
-    parser.add_argument("--gamma", type=float, default=1.0, help="Discount factor for Q learning")
-    parser.add_argument("--exploration-factor", type=float, default=0.3, help="Exploration factor for Q learning")
-    parser.add_argument("--init_granularity", type=int, default=50, help="Initial granularity of the Q table")
-    parser.add_argument("--envs", type=str, default="1,2,3,4,5", help="Environments to train on")
-    parser.add_argument("--q_table_savepath", type=str, default=os.path.join("dp", "q_table"), help="Path to save the Q table")
-    parser.add_argument("--wandb", action='store_true', help="Log to wandb")
-    parser.add_argument("--evaluate", action='store_true', help="Evaluate the Q table")
-    parser.add_argument("--train", action='store_true', help="Train the Q table")
-    parser.add_argument("--q_table_path", type=str, default=None, help="Path to the Q table to evaluate")
-    parser.add_argument("--start-pos", type=str, default=None, help="Starting position for training")
+    parser.add_argument(
+        "--nrollouts",
+        type=int,
+        default=int(1e6),
+        help="Number of rollouts to perform per upscaled table",
+    )
+    parser.add_argument(
+        "--nupscales", type=int, default=8, help="Number of times to upscale the table"
+    )
+    parser.add_argument(
+        "--upscale-factor", type=int, default=2, help="Factor to upscale the table by"
+    )
+    parser.add_argument(
+        "--lr", type=float, default=0.01, help="Learning rate for Q learning"
+    )
+    parser.add_argument(
+        "--gamma", type=float, default=1.0, help="Discount factor for Q learning"
+    )
+    parser.add_argument(
+        "--exploration-factor",
+        type=float,
+        default=0.3,
+        help="Exploration factor for Q learning",
+    )
+    parser.add_argument(
+        "--init_granularity",
+        type=int,
+        default=50,
+        help="Initial granularity of the Q table",
+    )
+    parser.add_argument(
+        "--envs", type=str, default="1,2,3,4,5", help="Environments to train on"
+    )
+    parser.add_argument(
+        "--q_table_savepath",
+        type=str,
+        default=os.path.join("dp", "q_table"),
+        help="Path to save the Q table",
+    )
+    parser.add_argument("--wandb", action="store_true", help="Log to wandb")
+    parser.add_argument("--wandb-project", type=str, default="upper-bound-2024-comp")
+    parser.add_argument("--wandb-entity", type=str, default="psaunder")
+    parser.add_argument("--evaluate", action="store_true", help="Evaluate the Q table")
+    parser.add_argument("--train", action="store_true", help="Train the Q table")
+    parser.add_argument(
+        "--q_table_path", type=str, default=None, help="Path to the Q table to evaluate"
+    )
+    parser.add_argument(
+        "--start-pos", type=str, default=None, help="Starting position for training"
+    )
     args = parser.parse_args()
 
     if args.train:
         train(vars(args))
 
     if args.evaluate:
-        assert os.path.isfile(args.q_table_path), f"Q Table path {args.q_table_path} does not exist"
+        assert os.path.isfile(
+            args.q_table_path
+        ), f"Q Table path {args.q_table_path} does not exist"
         evaluate(args.q_table_path, save=True)
